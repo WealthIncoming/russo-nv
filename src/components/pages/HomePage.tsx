684565b3517f6afafeee1c6a1c895f5e628c3252
Wix Remote Machine <support@wix.com>
2026-03-13 12:59:17 +0000

    Update Home page statistics

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -46,7 +46,7 @@ const INDUSTRIES_DATA = [
 ];
 
 const STATS_DATA = [
-  { valueKey: '30+', labelKey: 'statsYearsExperience', subKey: 'statsSince' },
+  { valueKey: '25+', labelKey: 'statsProjectsPerYear', subKey: 'statsAveragePerYear' },
   { valueKey: '1000+', labelKey: 'statsProjectsCompleted', subKey: 'statsIndustrialProjects' },
   { valueKey: '100%', labelKey: 'statsSafetyCertified', subKey: 'statsVcaIso' },
   { valueKey: '24/7', labelKey: 'Project Updates', subKey: 'Real-time Data' },
