32f0ad07cf1b3aab1b6ef3df367a1489696c9175
Wix Remote Machine <support@wix.com>
2026-03-13 11:30:04 +0000

    Manual code changes

--- src/lib/i18n/translations.ts
+++ src/lib/i18n/translations.ts
@@ -295,7 +295,7 @@ export const translations: Record<Language, Translations> = {
       statsCountriesServed: 'Landen Bediend',
       statsEuRegion: 'EU Regio',
       statsSafetyCertified: 'Veiligheidsgecertificeerd',
-      statsVcaIso: 'VCA / ISO',
+      statsVcaIso: 'VCA CERTIFIED',
       whyChooseUs: 'Waarom Russo NV Kiezen',
       whyChooseUsDesc: 'Decennia van expertise gecombineerd met geavanceerde veiligheidsnormen',
       expertise: 'Expertise',
