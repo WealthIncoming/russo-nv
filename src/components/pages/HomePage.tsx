10c313ed55915226b1e2d24439874feb4297cf01
Wix Remote Machine <support@wix.com>
2026-03-14 09:52:21 +0000

    Manual code changes

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -408,7 +408,7 @@ export default function HomePage() {
           <div className="text-center mb-24">
             <SectionLabel text={t('home', 'sectors')} align="center" />
             <h2 className="font-heading text-3xl sm:text-4xl md:text-6xl lg:text-7xl text-white leading-tight">
-              {t('home', 'builtForHeavyIndustry')}
+              {t('home', 'builtFor')} <span className="text-primary">{t('home', 'allIndustry')}</span>
             </h2>
           </div>
 
